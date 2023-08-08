01c65d93cc66cbde28b984d557cf6203e9effab0
Daniel Simoni Wegher <w.daniels@hotmail.com>
2023-08-08 11:32:43 -0300

    Inicializando Pasta do Projeto, incluindo Frameworks e Dependências Iniciais. Limpeza de arquivos. Inicio do Git Flow

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hello Team</h1> 
+      <p>Página APP para a criação das rotas</p>
+    </>
+  )
+}
+
+export default App
