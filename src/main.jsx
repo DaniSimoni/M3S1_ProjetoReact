73b4a0cc51951e4919fd131eeb916c373c308457
Daniel Simoni Wegher <w.daniels@hotmail.com>
2023-08-09 16:17:59 -0300

    Criando Header Component e Header Context

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-
+import { HeaderProvider } from './Context/Header.context.jsx'
 import { GlobalStyle} from './Global.style.jsx'
 
 
@@ -10,7 +10,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
   <React.StrictMode>
     <GlobalStyle/>
-
+    <HeaderProvider>
     <App/>
+    </HeaderProvider>
   </React.StrictMode>
 )
